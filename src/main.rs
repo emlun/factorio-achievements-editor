ee28ebc74e85d6374d782551f80c5b0d7581521a
Emil Lundberg <emil@emlun.se>
2025-11-15 21:32:42 +0100

    Add debug CLI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
+use std::ffi::OsString;
+use std::io::Read;
+
+use clap::Parser;
+use clap::Subcommand;
 use factorio_achievements_editor::AchievementsDat;
 use factorio_achievements_editor::AchievementsModdedDat;
 use factorio_achievements_editor::Parse;
 
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+
+    #[arg(long)]
+    modded: bool,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Dump,
+    Delete {
+        #[arg(value_name = "ID")]
+        id: OsString,
+    },
+}
+
 fn main() -> std::io::Result<()> {
-    // let data = AchievementsDat::parse(&mut std::io::stdin(), &())?;
-    let data = AchievementsModdedDat::parse(&mut std::io::stdin(), &())?;
-    dbg!(data);
+    let cli = Cli::parse();
+    let mut stdin = std::io::stdin();
+
+    match cli.command {
+        None | Some(Command::Dump) => {
+            if cli.modded {
+                let data = AchievementsModdedDat::parse(&mut stdin, &())?;
+                dbg!(data);
+            } else {
+                let data = AchievementsDat::parse(&mut stdin, &())?;
+                dbg!(data);
+            }
+        }
+        _ => unimplemented!(),
+    }
+
+    #[cfg(debug_assertions)]
+    dbg!({
+        let mut buf = Vec::new();
+        stdin.read_to_end(&mut buf)?;
+        buf
+    });
+
     Ok(())
 }
